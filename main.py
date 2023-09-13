468032ba6005e191415a32c9c18d911f44ea43af
nicholasconterno <77944074+nicholasconterno@users.noreply.github.com>
2023-09-13 23:02:59 +0000

    added markdowngenerator

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def plot_histogram_save(df, column_name, filename='histogram.png'):
     plt.close()  # Close the figure to free up memory
     return filename
 
+
 url = 'https://gist.githubusercontent.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea/raw/0c794a9717f18b094eabab2cd6a6b9a226903577/movies.csv'
 df = load_data(url)
 
@@ -51,8 +52,8 @@ print(get_summary_statistics(df))
 
 md = df.to_pandas().to_markdown()
 
-with open('generated_markdown.md','w') as f:
-    f.write(md)
+# with open('generated_markdown.md','w') as f:
+#     f.write(md)
 
 # Assuming the dataset has a column named 'Rotten Tomatoes %'
 print(f"Mean of 'Rotten Tomatoes %': {get_mean(df, 'Rotten Tomatoes %')}")
