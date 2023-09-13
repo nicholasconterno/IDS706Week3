0a50d46c04930b06e2434f10ce1f633b8d042eea
nicholasconterno <77944074+nicholasconterno@users.noreply.github.com>
2023-09-13 22:44:49 +0000

    changed pandas to polars

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import pandas as pd
+import polars as pl
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 def load_data(url):
-    df = pd.read_csv(url, skiprows=range(1, 32))  # Adjust the range as needed
+    df = pl.read_csv(url)
+    # Skipping the first 31 rows. 
+    df = df.slice(31, len(df) - 31)  
     return df
 
+
 def get_summary_statistics(df):
-    return df.describe()
+    return df.describe().to_pandas()
 
 def get_mean(df, column_name):
     return df[column_name].mean()
@@ -16,15 +21,16 @@ def get_median(df, column_name):
 def get_stdev(df, column_name):
     return df[column_name].std()
 
-import matplotlib.pyplot as plt
-
 def plot_histogram_save(df, column_name, filename='histogram.png'):
+    # Convert to pandas for plotting using seaborn and matplotlib
+    df_pandas = df.to_pandas()
+    
     # Using seaborn styles
     sns.set_style("whitegrid")
     
     # Create the histogram
     plt.figure(figsize=(10, 6))  # Set figure size for larger plot
-    sns.histplot(df[column_name], kde=True, color="dodgerblue", bins=30)  # KDE shows density curve
+    sns.histplot(df_pandas[column_name], kde=True, color="dodgerblue", bins=20)  # KDE shows density curve
     plt.title(f'Histogram of {column_name}', fontsize=18)
     plt.xlabel(column_name, fontsize=14)
     plt.ylabel('Density', fontsize=14)
@@ -37,21 +43,21 @@ def plot_histogram_save(df, column_name, filename='histogram.png'):
     plt.close()  # Close the figure to free up memory
     return filename
 
-
 url = 'https://gist.githubusercontent.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea/raw/0c794a9717f18b094eabab2cd6a6b9a226903577/movies.csv'
 df = load_data(url)
 
 # Print summary statistics
 print(get_summary_statistics(df))
 
-md = df.to_markdown()
+md = df.to_pandas().to_markdown()
+
 with open('generated_markdown.md','w') as f:
     f.write(md)
-# Assuming the dataset has a column named 'rating' 
-# (adjust column names based on the actual dataset columns)
+
+# Assuming the dataset has a column named 'Rotten Tomatoes %'
 print(f"Mean of 'Rotten Tomatoes %': {get_mean(df, 'Rotten Tomatoes %')}")
 print(f"Median of 'Rotten Tomatoes %': {get_median(df, 'Rotten Tomatoes %')}")
 print(f"Standard Deviation of 'Rotten Tomatoes %': {get_stdev(df, 'Rotten Tomatoes %')}")
 
-# Plotting a histogram for 'rating'
-fname= plot_histogram_save(df, 'Rotten Tomatoes %')
+# Plotting a histogram for 'Rotten Tomatoes %'
+fname = plot_histogram_save(df, 'Rotten Tomatoes %')
